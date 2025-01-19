0dde7f6039264d052d6389ef2a3a4299d43d2bc0
turuwo <turuwo3@gmail.com>
2025-01-19 14:22:21 +0900

    不要なコードを削除

--- app/src/main/java/com/pin/recommend/ui/main/StoryListFragment.kt
+++ app/src/main/java/com/pin/recommend/ui/main/StoryListFragment.kt
@@ -1,12 +1,7 @@
 package com.pin.recommend.ui.main
 
-import android.content.Intent
 import android.os.Bundle
-import android.text.SpannableString
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -15,17 +10,10 @@ import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
-import androidx.lifecycle.asLiveData
 import com.l4digital.fastscroll.FastScrollRecyclerView
-import com.pin.recommend.R
-import com.pin.recommend.domain.entity.RecommendCharacter
 import com.pin.recommend.ui.adapter.VerticalRecyclerViewAdapter
 import com.pin.recommend.ui.character.CharacterDetailsViewModel
 import com.pin.recommend.ui.character.CharacterDetailsViewModelState
-import com.pin.recommend.ui.story.StoryCreateActivity
-import com.pin.recommend.ui.story.StoryCreateActivity.Companion.INTENT_CHARACTER_ID
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.runBlocking
 
 class StoryListFragment : Fragment() {
     private var pageViewModel: PageViewModel? = null
@@ -44,8 +32,6 @@ class StoryListFragment : Fragment() {
             index = requireArguments().getInt(ARG_SECTION_NUMBER)
         }
         pageViewModel?.setIndex(index)
-
-        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
@@ -58,117 +44,10 @@ class StoryListFragment : Fragment() {
                 com.pin.recommend.ui.story.Content(requireActivity(), vm, state)
             }
         }
-        /*
-        val root = inflater.inflate(R.layout.fragment_story_list, container, false)
-
-        sortView = root.findViewById(R.id.sort)
-        sortView.setOnClickListener(View.OnClickListener {
-            val popup = PopupMenu(requireContext(), sortView)
-            popup.menuInflater.inflate(R.menu.sort_order, popup.menu)
-            popup.show()
-            popup.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item ->
-                when (item.itemId) {
-                    R.id.order_desc -> {
-                        vm.updateStorySortOrder(0)
-                        return@OnMenuItemClickListener true
-                    }
-
-                    R.id.order_asc -> {
-                        vm.updateStorySortOrder(1)
-                        return@OnMenuItemClickListener true
-                    }
-                }
-                true
-            })
-        })
-
-        verticalRecyclerViewAdapter = VerticalRecyclerViewAdapter(this, null)
-
-        vm.state.asLiveData().observe(requireActivity()) {
-            if (it == null) return@observe
-            sortView.setTextColor(it.appearance.homeTextColor)
-            val isAsc = it.storySortOrder == 1
-            if (isAsc) {
-                sortView.text = "並び順 : 登録日 古い順"
-            } else {
-                sortView.text = "並び順 : 登録日 新しい順"
-            }
-            it.character?.let {
-                initializeText(it.character)
-            }
-            verticalRecyclerViewAdapter.updateCharacter(it.character?.character)
-        }
-
-        vm.state.asLiveData().observe(requireActivity()) {
-            verticalRecyclerViewAdapter.setList(it.stories)
-        }
-
-        recyclerView = root.findViewById(R.id.story_recycle_view)
-        val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(activity)
-        recyclerView.addItemDecoration(
-            DividerItemDecoration(
-                requireContext(),
-                LinearLayoutManager(requireContext()).orientation
-            )
-        )
-        recyclerView.layoutManager = layoutManager
-        recyclerView.setHasFixedSize(false)
-        recyclerView.adapter = verticalRecyclerViewAdapter
-
-        vm.state.asLiveData().observe(viewLifecycleOwner) {
-            verticalRecyclerViewAdapter.setEditMode(
-                it.isDeleteModeStories
-            )
-        }
-
-        return root
-         */
-    }
-
-    private fun initializeText(character: RecommendCharacter) {
-        sortView.setTextColor(character.getHomeTextColor())
-        sortView.setShadowLayer(4f, 0f, 0f, character.getHomeTextShadowColor())
-        verticalRecyclerViewAdapter.updateCharacter(character)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.edit_mode, menu)
-        val editMode = menu.findItem(R.id.edit_mode)
-        vm.state.asLiveData().observe(this) {
-            val s: SpannableString = if (it.isDeleteModeStories) {
-                SpannableString("完了")
-            } else {
-                SpannableString("編集")
-            }
-            editMode.title = s
-        }
-
-        inflater.inflate(R.menu.create, menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.edit_mode -> {
-                vm.toggleEditModeStory()
-                return true
-            }
-
-            R.id.create -> {
-                runBlocking {
-                    val intent = Intent(activity, StoryCreateActivity::class.java)
-                    val characterId = vm.state.first().character?.id
-                    intent.putExtra(INTENT_CHARACTER_ID, characterId)
-                    startActivity(intent)
-                }
-            }
-        }
-        return true
     }
 
     companion object {
         private const val ARG_SECTION_NUMBER = "section_number"
-        const val INTENT_STORY = "com.pin.recommend.StoryFragment.INTENT_STORY"
 
         @JvmStatic
         fun newInstance(index: Int): StoryListFragment {
